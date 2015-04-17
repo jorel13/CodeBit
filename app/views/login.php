c5311bf534d931e32fd6aef5a87f1ff71e0b6640
lindarenee <lindarenee@gmail.com>
2015-04-17 12:18:22 -0500

    social page header

--- app/views/login.php
+++ app/views/login.php
@@ -1,78 +1,1 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8"/>
-
-    <title>Bits</title>
-
-
-    <meta name="app-route" content="<?PHP echo get_controller() . '/' . get_action(); ?>">
-
-    <meta name="app-url" content="<?PHP echo home_url(); ?>">
-
-
-
-    <?PHP echo asset('Bits.css'); ?>
-
-
-
-    <!--[if IE]>
-
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js"></script>
-
-    <![endif]-->
-
-
-</head>
-
-<body class="controller-<?PHP echo get_controller(); ?> action-<?PHP echo get_action(); ?>">
-
-
-<div id="auth" class="code-font">
-
-
-    <header>
-
-        <a href="http://coolbitsbro.com/"><span class="bit-embed"></span> Bits</a>
-
-    </header>
-
-    <?PHP if (isset($_GET['invalid'])) { ?><p>Invalid username or password</p><?PHP } ?>
-
-    <?PHP if (isset($_GET['loggedout'])) { ?><p>Catch ya later</p><?PHP } ?>
-
-    <form action="<?PHP echo home_url('auth/new'); ?>" method="POST">
-
-        <div>
-
-            <label>
-
-                <input autofocus="true" type="text" placeholder="Username" name="username" class="text">
-
-            </label>
-
-            <label>
-
-                <input type="password" placeholder="Password" name="password" class="text">
-
-            </label>
-
-        </div>
-
-        <div>
-
-            <input type="submit" class="pull-right btn btn-positive login-btn code-font" value="login">
-
-        </div>
-
-    </form>
-
-</div>
-
-
-
-<?PHP get_footer(); ?>
-
+<!DOCTYPE html><html lang="en"><head>    <meta charset="utf-8"/>    <title>Bits</title>    <meta name="app-route" content="<?PHP echo get_controller() . '/' . get_action(); ?>">    <meta name="app-url" content="<?PHP echo home_url(); ?>">    <?PHP echo asset('Bits.css'); ?>    <!--[if IE]>    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js"></script>    <![endif]--></head><body class="controller-<?PHP echo get_controller(); ?> action-<?PHP echo get_action(); ?>"><div id="auth" class="code-font">    <header>        <a href="http://usaa.com/"><span class="bit-embed"></span>eaglecode</a>    </header>    <?PHP if (isset($_GET['invalid'])) { ?><p>Invalid username or password</p><?PHP } ?>    <?PHP if (isset($_GET['loggedout'])) { ?><p>Catch ya later</p><?PHP } ?>    <form action="<?PHP echo home_url('auth/new'); ?>" method="POST">        <div>            <label>                <span>Username</span>                <input autofocus="true" type="text" name="username" class="text">            </label>            <label>                <span>Password</span>                <input type="password" name="password" class="text">            </label>        </div>        <div>            <input type="submit" class="btn btn-positive login-btn code-font" value="login">        </div>    </form>    <div class="main-content">        <h1>Popular Codelets</h1>        <div class="one-codelet">            <div class="codelet-info">                <div class="user-name">Lucille Bluth</div>                <div class="codelet-stats">                    <div class="hearts-num">42</div>                    <img src="" />                </div>            </div>            <div class="codelet-content iframe-wrap">                <a href="http://codepen.io/adriangyuricska/pen/bNXaBj"></a>                <iframe frameborder="0" data-src="http://s.codepen.io/adriangyuricska/fullcpgrid/bNXaBj" scrolling="no" allowtransparency="true" src="http://s.codepen.io/adriangyuricska/fullcpgrid/bNXaBj">                </iframe>                <img src="" />            </div>        </div>    </div></div><?PHP get_footer(); ?>
